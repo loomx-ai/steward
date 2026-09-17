20d52ac0cb91513e0fa90d7c1fe71cdcb91c7574
Prodesire <wangbinxin001@126.com>
2026-09-17 23:02:11 +0800

    style(gcp): format service account reference test

--- providers/gcp/service_account_references_test.go
+++ providers/gcp/service_account_references_test.go
@@ -10,7 +10,7 @@ func TestReferencesIncludeWorkloadServiceAccounts(t *testing.T) {
 	want := "//iam.googleapis.com/projects/proj/serviceAccounts/app@proj.iam.gserviceaccount.com"
 	defaultCompute := "//iam.googleapis.com/projects/proj/serviceAccounts/123456789-compute@developer.gserviceaccount.com"
 	for name, data := range map[string]map[string]any{
-		"compute instance": {"serviceAccounts": []any{map[string]any{"email": "app@proj.iam.gserviceaccount.com", "scopes": []any{"https://www.googleapis.com/auth/cloud-platform"}}}},
+		"compute instance":  {"serviceAccounts": []any{map[string]any{"email": "app@proj.iam.gserviceaccount.com", "scopes": []any{"https://www.googleapis.com/auth/cloud-platform"}}}},
 		"instance template": {"properties": map[string]any{"serviceAccounts": []any{map[string]any{"email": "app@proj.iam.gserviceaccount.com"}}}},
 		"gke node pool":     {"config": map[string]any{"serviceAccount": "app@proj.iam.gserviceaccount.com"}},
 		"cloud run":         {"template": map[string]any{"serviceAccount": "app@proj.iam.gserviceaccount.com"}},
